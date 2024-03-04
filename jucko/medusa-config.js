29adf3914516f08f805314148ac2e8d5890de0cf
Etukudoh Marvellous <Marvellous Etukudoh>
2024-03-04 23:25:18 +0100

    configured environment variables in backend

--- jucko/medusa-config.js
+++ jucko/medusa-config.js
@@ -44,6 +44,7 @@ const plugins = [
       webhook_secret: process.env.PAYSTACK_WEBHOOK_SECRET,
     },
   },
+  
   {
     resolve: `@medusajs/file-local`,
     options: {
@@ -118,7 +119,15 @@ const projectConfig = {
 
 /** @type {import('@medusajs/medusa').ConfigModule} */
 module.exports = {
-  projectConfig,
+  projectConfig:{
+    //...
+    database_extra: process.env.NODE_env !== "development" ?
+    {
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    } : {},
+  },
   plugins,
   modules,
   featureFlags: {
